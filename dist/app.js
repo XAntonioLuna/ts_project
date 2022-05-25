5cfd133f1be7a09bec7d4088d7b8ce2ee02c963f
XAntonioLuna <alunagonzalez@lyft.com>
2022-05-25 10:01:45 -0700

    May 25 progress

--- dist/app.js
+++ dist/app.js
@@ -1,90 +1,19 @@
 "use strict";
-class Department {
-    constructor(id, name) {
-        this.id = id;
+let add;
+add = (n1, n2) => {
+    return n1 + n2;
+};
+class Person {
+    constructor(name, age) {
         this.name = name;
-        this.employees = [];
+        this.age = age;
     }
-    static createEmployee(name) {
-        return { name: name };
-    }
-    addEmployee(employee) {
-        this.employees.push(employee);
-    }
-    printEmployeeInformation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
+    ;
+    greet(phrase) {
+        console.log(phrase + ' ' + this.name);
     }
 }
-Department.fiscalYear = 2022;
-class ITDepartment extends Department {
-    constructor(id, admins) {
-        super(id, 'IT');
-        this.admins = admins;
-    }
-    describe() {
-        console.log(`Department ${this.id}: ${this.name}`);
-    }
-}
-class AccountingDepartment extends Department {
-    constructor(id, reports) {
-        super(id, 'Accounting');
-        this.reports = reports;
-        this.lastReport = this.reports[0];
-    }
-    static getInstance() {
-        if (this.instance) {
-            return this.instance;
-        }
-        return new AccountingDepartment('AC', ['A report']);
-    }
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport;
-        }
-        throw Error('No last report');
-    }
-    set mostRecentReport(value) {
-        if (!value) {
-            throw Error('Invalid value');
-        }
-        this.addReports(value);
-    }
-    addEmployee(employee) {
-        if (employee === 'Max') {
-            return;
-        }
-        this.employees.push(employee);
-    }
-    addReports(text) {
-        this.reports.push(text);
-        this.lastReport = text;
-    }
-    printReports() {
-        console.log(this.reports);
-    }
-    describe() {
-        console.log(`Master Department ${this.id}: ${this.name}`);
-    }
-}
-const employee1 = Department.createEmployee('Sara');
-console.log(employee1);
-console.log(Department.fiscalYear);
-const it = new ITDepartment('IT', ['Barbie']);
-const accounting = AccountingDepartment.getInstance();
-accounting.addEmployee('Antonio');
-accounting.addEmployee('Barbie');
-accounting.describe();
-accounting.printEmployeeInformation();
-console.log(accounting.mostRecentReport);
-accounting.addReports('Report 1');
-accounting.printReports();
-accounting.mostRecentReport = 'Magical report';
-console.log(accounting.mostRecentReport);
-accounting.printReports();
-it.addEmployee('Paco');
-it.addEmployee('Victor');
-it.describe();
-console.log(accounting);
-console.log(it);
+let user1;
+user1 = new Person('Antonio', 33);
+user1.greet('My name is....WHAAAT??...');
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,126 +1,35 @@
-abstract class Department{
-    // private readonly id: string;
-    // private name:string;
-    static fiscalYear = 2022;
+// Alternative way to declare function types with interfaces
+interface AddFn {
+    (a: number, b:number): number;
+}
 
-    // Similar to other languages "protected" grants access within the class and its children
-    protected employees: string[] = [];
+let add: AddFn;
+add = (n1: number, n2:number) => {
+    return n1 + n2;
+}
+// ///////////////////////////////////////////////////////////
 
-    constructor(protected readonly id: string, protected name: string) {
-        // this.name = n;
-    }
+interface Named {
+    readonly name: string;
+    outputName? :string; // Optional property
 
-    static createEmployee(name: string) {
-        return {name: name};
-    }
-    // "this" is optional but it helps avoid error scenarios like the one described below
-    abstract describe(this: Department): void;
+    changeName?(n: string): void; // Optional method
+}
 
-    addEmployee(employee: string){
-        this.employees.push(employee);
-    }
+interface Greetable extends Named{
+    greet(phrase: string): void;
+}
 
-    printEmployeeInformation(){
-        console.log(this.employees.length);
-        console.log(this.employees);
+class Person implements Greetable {
+    
+    constructor(public name: string, public age: number) {};
+
+    greet(phrase: string) {
+        console.log(phrase + ' ' + this.name);
     }
 }
 
-class ITDepartment extends Department {
-    admins: string[];
+let user1: Greetable;
+user1 = new Person('Antonio', 33);
 
-    constructor(id: string, admins: string[]) {
-        // If we call super it MUST be before using "this"
-        super(id, 'IT');
-        this.admins = admins;
-    }
-
-    describe() {
-        console.log(`Department ${this.id}: ${this.name}`)
-    }
-}
-
-class AccountingDepartment extends Department {
-    private lastReport: string;
-    private static instance: AccountingDepartment;
-
-    static getInstance() {
-        // In a static environment "this" refers to the class not the instance
-        if (this.instance) {
-            return this.instance
-        }
-        return new AccountingDepartment('AC', ['A report']);
-    }
-
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport
-        }
-        throw Error('No last report');
-    }
-
-    set mostRecentReport(value: string) {
-        if (!value) {
-            throw Error('Invalid value');
-        }
-        this.addReports(value);
-    }
-
-    private constructor(id: string, private reports: string[]) {
-        super(id, 'Accounting');
-        this.lastReport = this.reports[0];
-    }
-
-    addEmployee(employee: string): void {
-        if (employee === 'Max') {
-            return;
-        }
-
-        this.employees.push(employee)
-    }
-
-    addReports(text: string) {
-        this.reports.push(text);
-        this.lastReport = text
-    }
-
-    printReports () {
-        console.log(this.reports);
-    }
-
-    describe() {
-        console.log(`Master Department ${this.id}: ${this.name}`)
-    }
-}
-
-const employee1 = Department.createEmployee('Sara');
-console.log(employee1);
-console.log(Department.fiscalYear);
-
-const it = new ITDepartment('IT', ['Barbie']);
-const accounting = AccountingDepartment.getInstance();
-
-accounting.addEmployee('Antonio');
-accounting.addEmployee('Barbie');
-accounting.describe();
-accounting.printEmployeeInformation();
-console.log(accounting.mostRecentReport);
-accounting.addReports('Report 1');
-accounting.printReports();
-
-accounting.mostRecentReport = 'Magical report';
-console.log(accounting.mostRecentReport);
-accounting.printReports();
-
-it.addEmployee('Paco');
-it.addEmployee('Victor');
-
-it.describe();
-
-/* To protect from this usage, we add "this" to the class' methods
-const accountingCopy = {describe: accounting.describe};
-accountingCopy.describe();
-*/
-
-console.log(accounting);
-console.log(it);
+user1.greet('My name is....WHAAAT??...')
